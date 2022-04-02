0b1ca3cb824c79d0373b50f9fb6610991293aa18
Matias Skjetne <matiaskj@samfundet.no>
2022-04-02 16:31:33 +0200

    Add random name generation on card creation

--- Assets/Scripts/Card Mechanic/DeckController.cs
+++ Assets/Scripts/Card Mechanic/DeckController.cs
@@ -5,11 +5,11 @@ public class DeckController : MonoBehaviour {
         public List<Card> PlayerCardPool = new List<Card>();
         public List<Card> EnemyCardPool = new List<Card>();
 
-        private string[] firstNames = new string[10];
-        private string[] lastNames = new string[10];
+        private string[] firstNames = {"Larry", "Davis"};
+        private string[] lastNames = {"Williams", "Paulson"};
+        private string[] flair = {"Of the flesh", "The destroyer", "The cuckholded"};
 
         private void Start() {
-
             PlayerCardPool = generateCardPool(10);
             EnemyCardPool  = generateCardPool(10);
             
@@ -29,7 +29,12 @@ public class DeckController : MonoBehaviour {
         }
 
         private Card generateCard(){
+            //Generate a random name for the card
+            string name = string.Format("{0} {1} {2}", firstNames[Random.Range(0,firstNames.Length)],
+            lastNames[Random.Range(0,lastNames.Length)],
+            flair[Random.Range(0,flair.Length)]);
 
-            return new Card("place", "holder", 1,1);
+            Debug.Log(name);
+            return new Card(name, "Generic card", 1,1);
         }
 }
