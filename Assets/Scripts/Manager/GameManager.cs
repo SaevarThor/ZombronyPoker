78e6fa5c8bef7bc42ebf8053ecd7cac78862b85a
Sævar Þór Pálsson <saevar1992@gmail.com>
2022-04-02 18:03:51 +0200

    Adding zombie and the initialization of fighting

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,9 +8,15 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     
     [SerializeField] private GameObject table;
-    private float combatInitTimer = 2;
+    [SerializeField] private Camera camera;
+    private float combatInitTimer = 3;
     private float dropHeight = 40;
 
+    private Zombie fightingZombie;
+    private Table fightingTable;
+    private Vector3 attackPosition;
+    private bool zoomCamera;
+    
     private void Awake()
     {
         if (Instance != this && Instance != null)
@@ -18,28 +25,58 @@ public class GameManager : MonoBehaviour
             Instance = this;
     }
 
-    public void RequestCombat(Vector3 playerPos, Vector3 zombiePos)
+    public void RequestCombat(Vector3 playerPos, Vector3 zombiePos, Zombie zombie)
     {
         float x = (playerPos.x + zombiePos.x) / 2;
         float y = (playerPos.y + zombiePos.y) / 2;
         float z = (playerPos.z + zombiePos.z) / 2;
-        Vector3 attackPosition = new Vector3(x, y, z);
-        attackPosition.y += dropHeight;
-        Instantiate(table, attackPosition, Quaternion.identity);
-        
+        attackPosition = new Vector3(x, y, z);
+        Vector3 tableSpawn = attackPosition;
+        tableSpawn.y += dropHeight;
+        GameObject g = Instantiate(table, tableSpawn, Quaternion.identity);
+        fightingTable = g.GetComponent<Table>(); 
         Player.Instance.SetMovement(false);
+
+        StartCoroutine(StartCombat());
     }
 
     private IEnumerator StartCombat()
     {
         yield return new WaitForSeconds(combatInitTimer);
         //Set camera target and zoom 
-        
+        //camera.transform.position = Vector3.Lerp(camera.StartPos, fightingTable.CameraPosition.position, 1); 
+        zoomCamera = true;
+
+
+        yield return new WaitForSeconds(5);
+
+        zoomCamera = false;
+
+        //INIT COMBAT
     }
 
-    public void EndCombat()
+    private void Update()
     {
-        //Kill off zombie and players. 
-        Player.Instance.SetMovement(true);
+        if (zoomCamera)
+        {
+            camera.transform.rotation = Quaternion.Slerp(camera.transform.rotation, Quaternion.LookRotation(attackPosition - camera.transform.position), 1*Time.deltaTime);
+            camera.orthographicSize -= (Time.deltaTime * 3);
+        }
+    }
+
+    public void EndCombat(bool win)
+    {
+        if (win)
+        {
+            //Kill off zombie and players. 
+            Player.Instance.SetMovement(true);
+            fightingZombie.Die();
+        }
+        else
+        {
+            fightingZombie.Celebrate();
+            //Kill Players
+            //End game
+        }
     }
 }
