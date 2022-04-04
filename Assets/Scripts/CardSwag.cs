b720a99077b3973c1200685ee0c5821cdcaa4797
Matias Skjetne <matiaskj@samfundet.no>
2022-04-04 18:14:39 +0200

    Flair now determine streangth of cards

--- Assets/Scripts/CardSwag.cs
+++ Assets/Scripts/CardSwag.cs
@@ -1,19 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public struct Flair {
+    public string flair;
+    public int damage;
+    public int health;
+}
 [CreateAssetMenu(fileName = "CardSwag", menuName = "Swag/SwaggySwagSwag")]
 public class CardSwag : ScriptableObject
 {
     public string[] FirstNames;
     public string[] LastNames;
-    public string[] Flairs;
+    public Flair[] Flairs;
     public Sprite[] Pictures;
     public string[] Description;
 
     public string[] ZombieNames;
     public string[] ZombieLastNames;
-    public string[] ZombieFliars;
+    public Flair[] ZombieFliars;
     public Sprite[] ZombiePictures;
     public string[] ZombieDescription;
 }
